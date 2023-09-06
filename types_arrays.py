960a8e80b31784b8f1154a773650a371f57a3ccd
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2023-09-06 13:29:40 +0530

    Create types_arrays.py
    types of array in python

---
+++ types_arrays.py
@@ -0,0 +1,10 @@
+
+my_list = [1, 2, 3, 4, 5]
+      #Accessing Elements: 
+      # Elements in a list are accessed by their index, starting from 0 for the first element.
+    first_element = my_list[0]  # Access the first element
+
+
+from array import array
+int_array = array('i', [1, 2, 3, 4, 5])
+
