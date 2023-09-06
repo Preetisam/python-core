a7d301508e88d174ee3caad161a2f57e357f9d7d
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2023-09-06 15:55:49 +0530

    Create method_array_6.py

---
+++ method_array_6.py
@@ -0,0 +1,3 @@
+my_list = [1, 2, 3, 2, 4, 2]
+count = my_list.count(2)  # Counting the number of occurrences of '2'
+print(count)             # Output: 3 (There are 3 occurrences of '2' in the list)
